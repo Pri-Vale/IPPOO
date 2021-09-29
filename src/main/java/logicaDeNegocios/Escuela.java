1484247a315d0b28be02af0ca3463d16f8c3542b
PriRamRo23 <pri2312@hotmail.com>
2021-09-28 21:52:38 -0600

    C

---
+++ src/main/java/logicaDeNegocios/Escuela.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logicaDeNegocios;
+
+/**
+ *
+ * @author pri23
+ */
+public class Escuela {
+    String hola;
+}
