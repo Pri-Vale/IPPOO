14fb11ef37111d71af988e4938d9d5ee4e8d50df
Vale <vuhleh27@gmail.com>
2021-09-28 22:50:36 -0600

    D

--- src/main/java/Connect_BD/Conexion.java
+++ src/main/java/Connect_BD/Conexion.java
@@ -33,7 +33,7 @@ public Connection Conectar_a_base(){
     String conectionURL= "jdbc:sqlserver://;database=GestorPlanesDeEstudio;integratedSecurity=true;";
     conect = DriverManager.getConnection(conectionURL);
         System.out.println("Hola");
-        System.out.println("Mundo");
+        System.out.println("Hola Mundo Hola Mundo");
     }
     catch (SQLException ex){
         System.out.println("Error.");
