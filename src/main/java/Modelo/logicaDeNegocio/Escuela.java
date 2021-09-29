b7767b9a056d84a73863fd36ee0906bfd1c32710
PriRamRo23 <pri2312@hotmail.com>
2021-09-29 14:05:05 -0600

    E

---
+++ src/main/java/Modelo/logicaDeNegocio/Escuela.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.logicaDeNegocio;
+
+/**
+ *
+ * @author pri23
+ */
+public class Escuela {
+    
+    String nombreEscuela;
+    String codEscuela; 
+
+    public Escuela() {
+    }
+    
+    public Escuela(String nombreEscuela, String codEscuela) {
+        this.nombreEscuela = nombreEscuela;
+        this.codEscuela = codEscuela;
+    }
+
+    public String getNombreEscuela() {
+        return nombreEscuela;
+    }
+
+    public void setNombreEscuela(String nombreEscuela) {
+        this.nombreEscuela = nombreEscuela;
+    }
+
+    public String getCodEscuela() {
+        return codEscuela;
+    }
+
+    public void setCodEscuela(String codEscuela) {
+        this.codEscuela = codEscuela;
+    }
+
+    @Override
+    public String toString() {
+        return "Escuela{" + "nombreEscuela=" + nombreEscuela + ", codEscuela=" + codEscuela + '}';
+    }
+    
+}
