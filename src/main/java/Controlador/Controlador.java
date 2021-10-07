86defd5e6912c3adf3fec1db14abd90c2c248a43
Vale <vuhleh27@gmail.com>
2021-10-07 16:27:16 -0600

    Prueba
    lo que sea que se vaya acá

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -1,5 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controlador;
@@ -31,4 +29,3 @@ public class Controlador {
         
     }
 }    
-        
