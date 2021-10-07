b585a4a03b184fb2b184b0638551dbab7697d661
PriRamRo23 <91581357+PriRamRo23@users.noreply.github.com>
2021-10-07 17:32:00 -0600

    Add files via upload

---
+++ IPPOO/src/main/java/SQLDatabaseConnection.java
@@ -0,0 +1,24 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLDatabaseConnection {
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    /*public static void main(String[] args) {
+        String connectionUrl =
+                "jdbc:sqlserver://;database=prueba;integratedSecurity=true;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+            System.out.println("Logrado");
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }*/
+    
+    
+}
