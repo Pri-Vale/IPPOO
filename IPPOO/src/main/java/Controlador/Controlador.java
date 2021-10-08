7a3df47e4f68aec41b7d0b67ae70e95b0ad37c8a
PriRamRo23 <pri2312@hotmail.com>
2021-10-07 18:12:22 -0600

    DESESPERACION2.0

---
+++ IPPOO/src/main/java/Controlador/Controlador.java
@@ -0,0 +1,32 @@
+package Controlador;
+
+import Connect_BD.Consultas_BaseDatos;
+import Modelo.logicaDeNegocio.Escuela;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author pri23
+ */
+public class Controlador {
+    
+    //Atributos 
+    private final Escuela escuela= new Escuela();
+    public static Consultas_BaseDatos salidaControlador =new Consultas_BaseDatos() ;
+    
+    public boolean crearEscuela(String pNombreEscuela, String pCodEscuela) throws SQLException{
+        try{
+        Escuela escuela= new Escuela(pNombreEscuela, pCodEscuela);
+            System.out.println(pNombreEscuela);
+            System.out.println(pCodEscuela);
+        salidaControlador.insertarEscuela(escuela);
+        System.out.println("A");
+        System.out.println("A");
+        return true;
+        
+        }catch(NullPointerException a){
+          return false;  
+        }                    
+        
+    }
+}    
